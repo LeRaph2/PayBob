ea8c9c5f88c6aa7ad96a43632e4443fe07af9b1f
Raphael Hanna <raphaelhanna@Mac.attlocal.net>
2025-07-25 18:16:10 -0500

    first commit

---
+++ PayBobTests/PayBobTests.swift
@@ -0,0 +1,17 @@
+//
+//  PayBobTests.swift
+//  PayBobTests
+//
+//  Created by Raphael Hanna on 7/25/25.
+//
+
+import Testing
+@testable import PayBob
+
+struct PayBobTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
